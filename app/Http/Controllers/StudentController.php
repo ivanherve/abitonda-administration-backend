e1529ea9f0cfef5ebae945995a4bf52225d62ac5
Ivan Herve <ivanherve1411@gmail.com>
2021-04-29 09:10:14 +0200

    add students through csv

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -30,7 +30,14 @@ class StudentController extends Controller
 
     public function getTenStudents(Request $request)
     {
-        $students = DB::table('vstudents')->paginate($request->get('limit'));
+        $students = DB::table('vstudents')->orderBy('Firstname', 'asc')->paginate($request->get('limit'));
+
+        return $this->successRes($students);
+    }
+
+    public function searchStudent(Request $request)
+    {
+        $students = DB::table('vstudents')->where('Firstname', 'like', $request->get('name') . "%")->get();
 
         return $this->successRes($students);
     }
@@ -68,7 +75,96 @@ class StudentController extends Controller
         return $this->successRes($newStudent);
     }
 
-    public function getParents(Request $request)
+    public function addStudentCSV(Request $request)
+    {
+        $file = $request->file('csv');
+        if (!$file) return $this->errorRes('Veuillez insérer un fichier', 404);
+        if ($file->getClientOriginalExtension() != 'csv') return $this->errorRes('Veuillez uniquement importer des fichiers .csv', 401);
+
+        $studentList = $this->csvToArray($file->getRealPath());
+        /**/
+        $sList = [];
+
+        foreach ($studentList as $k => $v) {
+            if (!$v['NOMS']) return $this->errorRes('Veuillez vérifier la ligne ' . ($k + 1) . ". Il manque un nom.", 404);
+            if (!$v['PRENOMS']) return $this->errorRes('Veuillez vérifier la ligne ' . ($k + 1) . ". Il manque un prénom.", 404);
+            if (!$v['DATE DE NAISSANCE']) return $this->errorRes('Veuillez vérifier la ligne ' . ($k + 1) . ". Il manque une date de naissance.", 404);
+            if (!$v['CANTINE']) return $this->errorRes('Veuillez vérifier la ligne ' . ($k + 1) . ". Est-ce que l'enfant mange à l'école ?", 404);
+            if (!$v['TRANSPORT']) return $this->errorRes('Veuillez vérifier la ligne ' . ($k + 1) . ". Est-ce que l'enfant est transporté ?", 404);
+            if (!$v['CLASSE']) return $this->errorRes('Veuillez vérifier la ligne ' . ($k + 1) . ". Il manque la classe.", 404);
+            $classe = Classe::all()->where('Name', '=', str_replace("\t", '', $v['CLASSE']))->first();
+            if (!$classe) return $this->errorRes("Veuillez vérifier " . strtoupper($v['PRENOMS']) . " " . strtoupper($v['NOMS']) . ". La classe de " . $v['Classe'] . " est introuvable", 404);
+            $existingStudentFirstname = Student::all()->where('Firstname', '=', $v['PRENOMS'])->first();
+            if ($existingStudentFirstname) {
+                $existingStudentLastname = Student::all()->where('Lastname', '=', $v['NOMS'])->first();
+                if ($existingStudentLastname) return $this->errorRes($v['PRENOMS'] . ' ' . $v['NOMS'] . ' existe déjà dans le système', 404);
+            } else Student::create([
+                'Firstname' => strtoupper(str_replace("\t", '', $v['PRENOMS'])),
+                'Lastname' => strtoupper(str_replace("\t", "", $v['NOMS'])),
+                'Birthdate' => date('Y-m-d', strtotime(str_replace('/', '-', str_replace("\t", '', $v['DATE DE NAISSANCE'])))),
+                'Canteen' => str_replace("\t", '', $v['CANTINE']) == 'Oui',
+                'Transport' => str_replace("\t", '', $v['TRANSPORT']) == 'Oui',
+                'ClasseId' => $classe->ClasseId,
+            ]);
+        }
+
+        //return $this->debugRes($sList);
+
+        return $this->successRes('la liste a bien été importé');
+    }
+
+    public function editStudent(Request $request)
+    {
+        $studentId = $request->input('studentId');
+        if (!$studentId) return $this->errorRes('De quel élève s\'agit-il ?', 404);
+
+        $student = Student::all()->where('StudentId', '=', $studentId)->first();
+
+        $firstname = $request->input('firstname');
+        if (!$firstname) $firstname = $student->Firstname;
+        $lastname = $request->input('lastname');
+        if (!$lastname) $lastname = $student->Lastname;
+        $birthdate = $request->input('birthdate');
+        if (!$birthdate) $birthdate = $student->Birthdate;
+        $classe = $request->input('classe');
+        $classeId = 0;
+        if (!$classe) $classeId = $student->ClasseId;
+        $allergies = $request->input('allergies');
+        if (!$allergies) $allergies = $student->allergies;
+        $canteen = filter_var($request->input('Canteen'), FILTER_VALIDATE_BOOLEAN);
+        $transport = filter_var($request->input('Transport'), FILTER_VALIDATE_BOOLEAN);
+        $registered = filter_var($request->input('Registered'), FILTER_VALIDATE_BOOLEAN);
+        $picture = $request->input('Picture');
+        if (!$picture) $picture = $student->Picture;
+
+        $classe = Classe::all()->where('Name', '=', $classe)->first();
+        if ($classe) $classeId = $classe->ClasseId;
+
+        $data = [
+            'Lastname' => strtoupper($lastname),
+            'Firstname' => strtoupper($firstname),
+            'Birthdate' => $birthdate,
+            'Canteen' => ($canteen != $student->Canteen) && $canteen,
+            'Transport' => ($transport != $student->Transport) && $transport,
+            'Picture' => $picture,
+            'ClasseId' => $classeId,
+            'Registered' => $registered,
+            'Allergies' => $allergies
+        ];
+        /*
+        return $this->debugRes([
+            '$data' => $data,
+            '$registered' => $registered,
+            'Canteen' => $canteen,
+            'Transport' => $transport,
+        ]);
+*/
+        $student->fill($data)->save();
+
+        return $this->successRes('Mis à jour réussi');
+    }
+
+    public function getStudentParents(Request $request)
     {
         $parents = Parents::all();
         $studentId = $request->get('studentid');
@@ -85,8 +181,8 @@ class StudentController extends Controller
         if (!$classe) return $this->errorRes('De quelle classe s\'agit-il ?', 404);
         $classe = strtoupper($classe);
 
-        $classeId = Classe::all()->where('Classe', '=', $classe)->pluck('ClasseId')->first();
-        //return $this->debugRes($classeId);
+        $classeId = Classe::all()->where('Name', '=', $classe)->pluck('ClasseId')->first();
+        //return $this->debugRes([$classeId, $classe]);
 
         $classe = DB::select('call get_students_per_classe(?);', [$classeId]);
         return $this->successRes($classe);
