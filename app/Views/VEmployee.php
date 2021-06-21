7ce086dad7cda114304abee557948accc87c4728
Ivan Herve <ivanherve1411@gmail.com>
2021-06-21 10:32:30 +0200

    View Employee

---
+++ app/Views/VEmployee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Views;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class VEmployee extends Model
+{
+    protected $table = 'vemployee';
+    protected $primaryKey = 'EmployeeId';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'EmployeeId', 'Firstname', 'Lastname', 'Email', 'Bank', 'BankAccount', 'NbRSSB', 'NbDays'
+    ];
+}
