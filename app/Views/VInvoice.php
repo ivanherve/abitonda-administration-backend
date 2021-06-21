da0d829b56bca319a9f6058398f138b2de165ef2
Ivan Herve <ivanherve1411@gmail.com>
2021-06-21 10:32:44 +0200

    View Invoice

---
+++ app/Views/VInvoice.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Views;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class VInvoice extends Model
+{
+    protected $connection = 'mysql2';
+    protected $table = 'vinvoice';
+    protected $primaryKey = 'InvoiceId';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'InvoiceId', 'Title', 'Amount', 'DatePayment', 'Description', 'BillPicture', 'PaymentMethod', 'created_at', 'updated_at'
+    ];
+}
