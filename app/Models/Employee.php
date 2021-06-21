f23ab19f9d073bf136f0a4e61a433d225c9c2003
Ivan Herve <ivanherve1411@gmail.com>
2021-06-21 10:30:39 +0200

    model employee

---
+++ app/Models/Employee.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $table = 'employee';
+    protected $primaryKey = 'EmployeeId';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'EmployeeId', 'Firstname', 'Lastname', 'Email', 'BankId', 'BankAccount', 'NbRSSB', 'NbDays'
+    ];
+}
