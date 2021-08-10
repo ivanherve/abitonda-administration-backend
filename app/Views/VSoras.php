fc505eedb949a65a8c970e578bae9ecf618bd5a4
Ivan Herve <ivanherve1411@gmail.com>
2021-08-10 14:30:35 +0200

    soras list

---
+++ app/Views/VSoras.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Views;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class VSoras extends Model
+{
+    protected $table = 'vsoras';
+    protected $primaryKey = 'StudentId';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'StudentId', 'Lastname', 'Firstname', 'Birthdate', 'Classe'
+    ];
+}
