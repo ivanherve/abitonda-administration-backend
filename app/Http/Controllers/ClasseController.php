e17dd1ebd1da1f52e2abea1073932f9d5c677588
Ivan Herve <ivanherve1411@gmail.com>
2021-08-16 11:16:15 +0200

    add & remove teacher or assistant

--- app/Http/Controllers/ClasseController.php
+++ app/Http/Controllers/ClasseController.php
@@ -31,6 +31,50 @@ class ClasseController extends Controller
 
         $oneClass['assistants'] = DB::select("call get_assistants(?)", [$oneClass->ClasseId]);
 
-        return $this->successRes([$oneClass]);
+        return $this->successRes($oneClass);
+    }
+
+    public function AddTeacher(Request $request)
+    {
+        $employeeId = $request->input('employeeId');
+        $classeId = $request->input('classeId');
+        if (DB::insert("call add_teacher(?,?)", [$employeeId, $classeId]))
+            return $this->successRes("L'enseignant a bien été retiré");
+        else return $this->errorRes("L'enseignant n'a pas pu être ajouté", 500);
+    }
+
+    public function RemoveTeacher(Request $request)
+    {
+        $employeeId = $request->input('employeeId');
+        $classeId = $request->input('classeId');
+        if (DB::delete("call remove_teacher_from_classe(?,?)", [$employeeId, $classeId]))
+            return $this->successRes("L'enseignant a bien été retiré");
+        else return $this->errorRes("L'enseignant n'a pas pu être retiré", 500);
+    }
+
+    public function AddAssistant(Request $request)
+    {
+        $employeeId = $request->input('employeeId');
+        $classeId = $request->input('classeId');
+        if (DB::insert("call add_assistant(?,?)", [$classeId, $employeeId]))
+            return $this->successRes("L'enseignant a bien été retiré");
+        else return $this->errorRes("L'enseignant n'a pas pu être ajouté", 500);
+    }
+
+    public function RemoveAssistant(Request $request)
+    {
+        $employeeId = $request->input('employeeId');
+        $classeId = $request->input('classeId');
+        if (DB::delete("call remove_assistant_from_classe(?,?)", [$classeId, $employeeId]))
+            return $this->successRes("L'assistant a bien été retiré");
+        //else return $this->errorRes("L'assistant n'a pas pu être retiré", 500);
+    }
+
+    public function getListContactPerClasse(Request $request)
+    {
+        $classe = $request->get('classe');
+        $contacts = DB::select("call list_contact_per_classe(?)", [$classe]);
+
+        return $this->successRes($contacts);
     }
 }
